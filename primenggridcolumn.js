bc483c0a17cd53591570c7121908b8990585d259
manojsharma007 <manojsharmamca@hotmail.com>
2018-11-09 19:56:15 +0530

    Add files via upload

---
+++ primenggridcolumn.js
@@ -0,0 +1,36 @@
+
+
+ describe('MUltiple Scenarios', function() {
+    // Global variable
+    var globelSearchText= element(by.css('.globelSearchText'));
+   
+    //beforeach -- > will be executed before it block
+    beforeEach(function(){ 
+      
+        browser.get('http://localhost:4200/');
+    });
+      
+        it('Enter the text in globel serach box ', function() {          
+            
+            var tabledata = element.all(by.xpath("/html/body/app-root/p-table/div/div[2]/table/tbody"));            
+            var cells = tabledata.all(by.tagName('td'));  
+            var cellTexts = cells.map(function (elm) {                
+                elm.getText().then(function(aa){
+                    console.log(aa);
+                });                
+            });
+
+          });
+          // count the page
+          
+          var elements = element.all(protractor.By.css('span a'));         
+          elements.count().then(function(aa){
+              console.log(aa);
+          });
+
+ 
+    
+    
+  });
+
+
